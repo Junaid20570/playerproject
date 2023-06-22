3688e3de25ad4e479557f701497043d2d2a36248
Sheik Junaid <p7167254@ness.com>
2023-06-22 16:12:41 +0530

    'staticip'

--- src/app/player.service.ts
+++ src/app/player.service.ts
@@ -10,22 +10,22 @@ export class PlayerService {
   constructor(private _httpClient:HttpClient) { }
 
   public getPlayers() {
-    return this._httpClient.get<Player[]>("http://16.16.192.236:9090/player");
+    return this._httpClient.get<Player[]>("http://16.16.234.165:9090/player");
   }
 
   public setPlayers(player:Player) {
-    return this._httpClient.post<Player>("http://16.16.192.236:9090/player",player);
+    return this._httpClient.post<Player>("http://16.16.234.165:9090/player",player);
   }
 
   public pendingPlayers() {
-    return this._httpClient.get<Player[]>("http://16.16.192.236:9090/player?status=pending");
+    return this._httpClient.get<Player[]>("http://16.16.234.165:9090/player?status=pending");
   }
 
   public allotPlayer(player:Player) {
-    return this._httpClient.put<string>("http://16.16.192.236:9090/player?status=pending",player,{responseType: 'text' as 'json'});
+    return this._httpClient.put<string>("http://16.16.234.165:9090/player?status=pending",player,{responseType: 'text' as 'json'});
   }
 
   public deletePlayer(id:string){
-    return this._httpClient.delete<string>(`http://16.16.192.236:9090/player/${id}`,{responseType: 'text' as 'json'});
+    return this._httpClient.delete<string>(`http://16.16.234.165:9090/player/${id}`,{responseType: 'text' as 'json'});
   }
 }
